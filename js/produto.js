f9622c0337c773b838537eac9977f680bc4cd783
chinagliadev <156460261+chinagliadev@users.noreply.github.com>
2026-02-14 22:57:06 -0300

    implementa modal de deletar

--- js/produto.js
+++ js/produto.js
@@ -1,17 +1,19 @@
 const modal = document.getElementById('modalProduto')
 
 modal.addEventListener('show.bs.modal', ()=>{
-    console.log('ola')
 })
 
 const modalDetalhes = document.getElementById('modalDetalhesProduto')
 
 modalDetalhes.addEventListener('show.bs.modal', ()=>{
-    console.log('ola')
 })
 
 const modalEditar = document.getElementById('modalEditarProduto')
 
 modalEditar.addEventListener('show.bs.modal', ()=>{
-    console.log('oii')
+})
+
+const modalDesativarProduto = document.getElementById('modalDesativarProduto')
+
+modalDesativarProduto.addEventListener('show.bs.modal', ()=>{
 })
