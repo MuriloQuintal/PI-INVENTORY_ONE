04c771738347b955437c3b16f798cd14f4bd9359
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2026-03-30 21:33:48 -0300

    SENHAHASH

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -11,12 +11,20 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-
+const bcrypt = require('bcrypt')
+async function geraHash(senha) {
+    const salts = 12;
+    const senhaHash = await bcrypt.hash(senha, salts);
+    return senhaHash;
+    
+}
 const conexao = require('../banco_dados/conexaoBanco')
 
 
-app.post("/cadastrousuarios/", function (req, res) {
+app.post("/cadastrousuarios/", async function (req, res) {
     const data = req.body;
+    const senhaCriptografada = await geraHash(data.senha);
+    data.senha = senhaCriptografada
     conexao.query(`INSERT INTO usuarios set ?`, [data],
         function (erro, resultado) {
             if (erro) {
