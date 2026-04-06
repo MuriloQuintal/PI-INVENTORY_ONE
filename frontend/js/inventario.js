d1c65a57c23b398bdfc50d3f3959c63df2d4640c
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2026-04-05 22:21:22 -0300

    Login validado url ocultada com localStorage

--- frontend/js/inventario.js
+++ frontend/js/inventario.js
@@ -1,3 +1,7 @@
+if (localStorage.getItem("logado") != "true") {
+    window.location.href = "login.html"
+}
+
 function fnListarInventarios() {
     fetch(`http://localhost:3000/inventarios`, { method: "GET" })
         .then(resultado => resultado.json())
